ac2df05b9d4b194fe708bad69d92e91ac0d1deb2
Rodrigo Costa <rodrigoc94@hotmail.com>
2024-12-26 10:46:00 -0300

    Ajustes testes unitários

--- tests/test_mega_sena_checker.py
+++ tests/test_mega_sena_checker.py
@@ -40,5 +40,11 @@ class TestMegaSenaChecker(unittest.TestCase):
         resultados = conferir_resultados(numeros_sorteados, jogos)
         self.assertEqual(resultados, [(1, 6), (3, 4)])
 
+    def test_conferir_resultados_jogos_com_15_numeros(self):
+        numeros_sorteados = [2, 4, 6, 8, 10, 12]
+        jogos = [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], [1, 4, 6, 8, 10, 13, 14, 15, 16, 17, 18, 20]]
+        resultados = conferir_resultados(numeros_sorteados, jogos)
+        self.assertEqual(resultados, [(1, 6), (2, 4)])
+
 if __name__ == "__main__":
     unittest.main()
